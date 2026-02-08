a4b37c9fcb217194c5885321aaa820edd83821a4
Trubchaninov Andrii <112893271+Rey-ui@users.noreply.github.com>
2026-02-08 12:17:01 +0200

    add swiper

--- js/main.js
+++ js/main.js
@@ -2,8 +2,28 @@ const burgerMenu = document.querySelector(".header-burger");
 const modalEl = document.querySelector(".modal");
 const linkEl = document.querySelector(".modal-close");
 burgerMenu.addEventListener("click", () => {
-    modalEl.classList.add("active");
-})
+  modalEl.classList.add("active");
+});
 linkEl.addEventListener("click", () => {
-    modalEl.classList.remove("active");
-})
+  modalEl.classList.remove("active");
+});
+// Initialize Swiper
+const swiper = new Swiper(".my-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 18,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1136: {
+      slidesPerView: 4,
+    },
+  },
+});
